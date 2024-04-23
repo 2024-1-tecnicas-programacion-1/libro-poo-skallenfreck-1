2dbae8ee44517abf3bf3e483a975719e2b62157e
Alexander Chacon <fresnedaalexander@gmail.com>
2024-04-22 22:12:45 -0500

    Desarrollo 1

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,103 @@
 package com.mycompany.libropoo;
 
+import java.util.Calendar;
+
+/**
+ * clase Libro
+ *
+ * @author USUARIO
+ */
 public class Libro {
-    
+
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int anoPublicacion = 0;
+    private boolean leido;
+
+    Calendar cal = Calendar.getInstance();
+    int year = cal.get(Calendar.YEAR);
+
+    /**
+     * Metodo constructor para inicialiizar los atributos de la clase
+     *
+     * @param titulo
+     * @param autor
+     * @param anoPublicacion
+     * @param leido
+     * @param genero
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+     */
+    public Libro(String titulo, String autor, int anoPublicacion, boolean leido, String genero) {
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anoPublicacion = anoPublicacion;
+        this.leido = leido;
+
+    }
+
+    /**
+     * Metodo que imprime las variables titulo, autor, anoPublicacion y genero
+     *
+     * @return String "Título: ["+titulo+"], Autor: ["+autor+"], Año de
+     * publicación: ["+anoPublicacion+"], Género: ["+genero+"]"
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+     */
+    public String mostrarInformacion() {
+        return "Título: [" + titulo + "], Autor: [" + autor + "], Año de publicación: [" + anoPublicacion + "], Género: [" + genero + "]";
+    }
+
+    /**
+     * Metodo que cambia el valor de la variable leido por true
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+     */
+    public void marcarLeido() {
+        leido = true;
+    }
+
+    /**
+     * Metodo que verifica si el libro se considera antiguo o no
+     *
+     * @return true // false
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+     */
+    public boolean esAntiguo() {
+        return (year - anoPublicacion) > 50;
+    }
+
+    /**
+     * Metodo que imprime por pantalla si el libro fue leído o no
+     *
+     * @return "El libro ya fue leído" // "El libro no ha sido leído"
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+     */
+    public String libroLeido() {
+        if (leido == true) {
+            return "El libro ya fue leído";
+        } else {
+            return "El libro no ha sido leído";
+        }
+    }
+
+    /**
+     * Metodo que imprime por pantalla si el libro es antiguo o no
+     *
+     * @return "El libro es Antiguo" // "El libro no es Antiguo"
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+     */
+    public String antiguo() {
+        if (esAntiguo() == true) {
+            return "El libro es Antiguo";
+        } else {
+            return "El libro no es Antiguo";
+        }
+    }
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,35 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
+/**
+ * Clase principal
+ */
 public class Principal {
-    
+
+    /**
+     * Metodo inicial que es ejecutado
+     *
+     * @param args
+     *
+     * Complejidad tenporal: o(1) Tiempo constante
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean leido = false;
+        System.out.println("Por favor ingrese el título del libro");
+        String titulo = sc.nextLine();
+        System.out.println("Por favor ingrese el autor del libro");
+        String autor = sc.nextLine();
+        System.out.println("Por favor ingrese el género del libro");
+        String genero = sc.nextLine();
+        System.out.println("Por favor ingrese el año de publicación del libro");
+        int anoPublicacion = sc.nextInt();
+
+        Libro datosLibro = new Libro(titulo, autor, anoPublicacion, leido, genero);
+        System.out.println(datosLibro.mostrarInformacion());
+        datosLibro.marcarLeido();
+        System.out.println(datosLibro.libroLeido());
+        System.out.println(datosLibro.antiguo());
+    }
 }
